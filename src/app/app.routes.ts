4e236bd15904da385106ff0f5a6e6147cd912fb3
sagarmasal12 <sagarmasal12@gmail.com>
2025-10-30 17:53:27 +0530

    app route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { App } from './app';
+import { EmployeeForm } from './pages/employee-form/employee-form';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'',
+        component:App
+    },
+    {
+        path:'Employee-Form',
+        component:EmployeeForm
+    }
+];
