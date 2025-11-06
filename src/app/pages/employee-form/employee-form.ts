b77980492ff0c69593d0852a0e8a15d08a7ab78a
sagarmasal12 <sagarmasal12@gmail.com>
2025-11-06 11:44:23 +0530

    reactiveform

--- src/app/pages/employee-form/employee-form.ts
+++ src/app/pages/employee-form/employee-form.ts
@@ -1,17 +1,49 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { EmpService } from '../../service/emp-service';
+import { EmailValidator, FormBuilder, FormGroup, NgModel, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-employee-form',
-  imports: [],
+  imports: [ReactiveFormsModule,CommonModule],
   templateUrl: './employee-form.html',
-  styleUrl: './employee-form.css',
+  styleUrls: ['./employee-form.css'],
 })
 export class EmployeeForm implements OnInit{
 
-  employeeSrv = inject(EmpService);
+  employees :any[]=[];
+  empForm!:FormGroup;
+
+  constructor(private empService:EmpService, private fb:FormBuilder){
+
+  }
+
 
   ngOnInit(): void {
-    
+    this.GetAllEmpl();
+    this.empForm =this.fb.group({
+      firstName:['',Validators.required],
+      lastName:['',Validators.required],
+      email:['',Validators.required],
+      salary:['',Validators.required],
+      department:['',Validators.required]
+    })
   }
+
+  GetAllEmpl(){
+    this.empService.getAllEmp().subscribe({
+      next:(res:any)=>{
+        console.log('Api Responce',res);
+        this.employees= res.data.recordset
+        console.log(this.employees);
+        
+      },
+      error:(err)=>{
+        console.error("Api Error",err);
+      }
+     
+    })
+  }
+
+  
 }
