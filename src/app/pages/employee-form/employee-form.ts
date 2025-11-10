3120cf5a9c6266642cc73dc530b20649e4f4f298
sagarmasal12 <sagarmasal12@gmail.com>
2025-11-10 14:54:25 +0530

    Refactor employee form and service: update form controls to use snake_case, add form submission logic, and improve API interaction methods.

--- src/app/pages/employee-form/employee-form.ts
+++ src/app/pages/employee-form/employee-form.ts
@@ -1,49 +1,97 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { EmpService } from '../../service/emp-service';
-import { EmailValidator, FormBuilder, FormGroup, NgModel, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  EmailValidator,
+  FormBuilder,
+  FormGroup,
+  NgModel,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-employee-form',
-  imports: [ReactiveFormsModule,CommonModule],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './employee-form.html',
   styleUrls: ['./employee-form.css'],
 })
-export class EmployeeForm implements OnInit{
+export class EmployeeForm implements OnInit {
+  employees: any[] = [];
+  empForm!: FormGroup;
+  editMode:boolean = false;
 
-  employees :any[]=[];
-  empForm!:FormGroup;
 
-  constructor(private empService:EmpService, private fb:FormBuilder){
 
-  }
-
+  constructor(private empService: EmpService, private fb: FormBuilder) {}
 
   ngOnInit(): void {
     this.GetAllEmpl();
-    this.empForm =this.fb.group({
-      firstName:['',Validators.required],
-      lastName:['',Validators.required],
-      email:['',Validators.required],
-      salary:['',Validators.required],
-      department:['',Validators.required]
+    
+    this.empForm = this.fb.group({
+      first_name: ['', Validators.required],
+      last_name: ['', Validators.required],
+      email: ['', Validators.required],
+      salary: ['', Validators.required],
+      department: ['', Validators.required],
+    });
+  }
+
+  GetAllEmpl() {
+    this.empService.getAllEmp().subscribe({
+      next: (res: any) => {
+        console.log('Api Responce', res);
+        this.employees = res.data.recordset;
+        console.log(this.employees);
+      },
+      error: (err) => {
+        console.error('Api Error', err);
+      },
+    });
+  }
+
+  onSubmit() {
+    if (this.empForm.valid) {
+      const formData = this.empForm.value;
+      this.empService.addEmp(formData).subscribe({
+        next: (res) => {
+          console.log('EMployee added succesffully', res);
+          this.empForm.reset();
+          this.GetAllEmpl();
+          
+        },
+        error: (err) => {
+          console.error('Invalid msgerrror', err);
+        },
+      });
+    }else{
+      console.log("Form Invalid")
+    }
+  
+  }
+
+  onEdit(){
+   
+    this.editMode == true;
+    this.empForm.patchValue({
+      
     })
   }
 
-  GetAllEmpl(){
-    this.empService.getAllEmp().subscribe({
+  deleteEmployee(empId:number){
+    debugger;
+    console.log("This is process for deleteAPi",empId)
+    this.empService.deleteEmp(empId).subscribe({
       next:(res:any)=>{
-        console.log('Api Responce',res);
-        this.employees= res.data.recordset
-        console.log(this.employees);
-        
+        alert(res.message);
+        this.GetAllEmpl();
       },
       error:(err)=>{
-        console.error("Api Error",err);
+        console.error("Delete Api Error:",err)
       }
-     
     })
   }
 
-  
+ 
 }
--- src/app/service/emp-service.ts
+++ src/app/service/emp-service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -11,6 +12,15 @@ export class EmpService {
   apiUrl:string = "http://localhost:4208"
 
   getAllEmp(){
-    return this.http.get(this.apiUrl+"/getAll")
+    return this.http.get(this.apiUrl+"/getall")
   }
+
+  addEmp(data:any):Observable<any>{
+    return this.http.post(`${this.apiUrl}/addEmp`,data);
+  }
+
+  deleteEmp(empId:number):Observable<any>{
+    return this.http.delete(`${this.apiUrl}/deleteEmp/${empId}`)
+  }
+  
 }
